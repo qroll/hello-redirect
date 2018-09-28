e78c7c9e63c968ade7b61b73a5fa12277f414143
Quek Ruo Ling <>
2018-09-28 12:29:22 +0800

    gitignore

--- src/redirect.js
+++ src/redirect.js
@@ -1,9 +1,35 @@
 import express from 'express';
 import cookieSession from 'cookie-session';
 import path from 'path';
+import helmet from 'helmet';
 
 const app = express();
 const port = 9000;
+
+const csp = {
+  self: `'self'`,
+  unsafeInline: `'unsafe-inline'`,
+  unsafeEval: `'unsafe-eval'`,
+  scripts: [
+  ],
+  images: [
+  ],
+  child: [
+  ],
+  styles: [
+    'blob:'
+  ]
+}
+
+const getDirectives = () => {
+  return {
+    defaultSrc: [csp.self],
+    imgSrc: [csp.self, ...csp.images],
+    formAction: [csp.self],
+    connectSrc: [csp.self, ...csp.scripts]
+    // 'report-uri': '/csp/report',
+  }
+}
 
 const sleep = secs => new Promise(resolve => {
   setTimeout(() => {
@@ -11,17 +37,31 @@ const sleep = secs => new Promise(resolve => {
   }, secs * 1000)
 })
 
+app.use(helmet());
+// app.use(helmet.contentSecurityPolicy({
+//   directives: getDirectives()
+// }));
+app.set('view engine', 'ejs');
+
+app.get('*', (req, res, next) => {
+  console.log(req.url);
+  next();
+})
+
 app.use(cookieSession({
+  name: 'session',
   secret: 'this is secret',
-  path: '/',
   maxAge: 1000 * 60 * 10,
-  sameSite: 'lax',
-  httpOnly: false
+  sameSite: 'lax'
 }));
 
 app.get('/redirect', (req, res) => {
+  res.header('Set-Cookie', 'sessionType=active; Path=/');
   req.session = { myCookie: new Date() }
-  res.redirect('/');
+
+  res.setHeader('Location', 'https://337e726f.ngrok.io/');
+  res.status(302);
+  res.json({});
 });
 
 app.get('/ok', (req, res) => {
@@ -31,6 +71,7 @@ app.get('/ok', (req, res) => {
 });
 
 app.get('/', async (req, res) => {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
